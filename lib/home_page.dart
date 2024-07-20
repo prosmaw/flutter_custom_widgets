3a7e3885aae93a4ba567270489f7af6b07a9e952
Prosper AMEBE <prosperamebe14@gmail.com>
2024-07-19 23:45:52 -0400

    ...

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:custom_widgets/widgets/angular_gradient_button.dart';
+import 'package:custom_widgets/widgets/gradient_border.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,67 +12,89 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   late final AnimationController _animationController = AnimationController(
-    duration: const Duration(seconds: 5),
+    duration: const Duration(seconds: 15),
     vsync: this,
-  )
-    ..forward()
-    ..repeat(reverse: false);
-  late final Animation<double> _animation =
-      Tween<double>(begin: 0.0, end: 4.0).animate(_animationController);
+  )..repeat();
 
-  Gradient borderGradient = const LinearGradient(colors: [
-    Colors.red,
-    Colors.orange,
-    Colors.yellow,
-    Colors.green,
-    Colors.blue,
-    Colors.purple,
-    Colors.pink
-  ]);
+  late final Animation<double> _turnAnimation =
+      Tween<double>(begin: 0.0, end: 4.0).animate(_animationController);
+  Gradient get borderGradient => const LinearGradient(
+        colors: [
+          Colors.red,
+          Colors.orange,
+          Colors.yellow,
+          Colors.green,
+          Colors.blue,
+          Colors.purple,
+          Colors.pink
+        ],
+      );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            RotationTransition(
-              turns: _animation,
-              child: CustomPaint(
-                size: const Size(280, 60),
-                painter: GradientBorder(
-                  gradient: borderGradient,
-                  radius: const Radius.circular(20),
-                  strokeWidth: 3,
-                ),
-              ),
-            )
-          ],
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text(
+          "Custom widgets",
+          style: TextStyle(color: Colors.white),
         ),
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Center(
+            child: AngularGradientButton(),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Stack(
+            children: [
+              GradientBorder(
+                  gradient: borderGradient,
+                  radius: 180,
+                  size: const Size(150, 150),
+                  strokeWidth: 25),
+              RotationTransition(
+                turns: _turnAnimation,
+                child: CustomPaint(
+                  size: const Size(150, 150),
+                  painter: CustomArc(
+                    color: const Color.fromARGB(255, 58, 56, 56),
+                    strokeWidth: 25,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
 }
 
-class GradientBorder extends CustomPainter {
-  final Radius radius;
+class CustomArc extends CustomPainter {
   final double strokeWidth;
-  final Gradient gradient;
+  final Color color;
   final _paint = Paint()..style = PaintingStyle.stroke;
 
-  GradientBorder(
-      {required this.gradient,
-      required this.radius,
-      required this.strokeWidth});
+  CustomArc({required this.color, required this.strokeWidth});
 
   @override
   void paint(Canvas canvas, Size size) {
-    final rect = Rect.fromLTWH(0, 0, size.width, size.height);
-    final rrect = RRect.fromRectAndRadius(rect, radius);
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+
+    final rect = Rect.fromCircle(center: center, radius: radius);
     _paint
       ..strokeWidth = strokeWidth
-      ..shader = gradient.createShader(rect);
-    canvas.clipRRect(rrect);
+      ..strokeCap = StrokeCap.round
+      ..strokeJoin = StrokeJoin.round
+      ..color = color;
+    canvas.drawArc(rect, -math.pi / 2, -math.pi * 7 / 4, false, _paint);
   }
 
   @override
---
+++ lib/widgets/gradient_border.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class GradientBorder extends StatefulWidget {
+  const GradientBorder(
+      {super.key,
+      required this.gradient,
+      required this.radius,
+      required this.size,
+      required this.strokeWidth});
+  final Gradient gradient;
+  final double radius, strokeWidth;
+  final Size size;
+  @override
+  State<GradientBorder> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<GradientBorder> {
+  @override
+  void dispose() {
+    //_animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: widget.size,
+      painter: BorderPainter(
+        gradient: widget.gradient,
+        radius: Radius.circular(widget.radius),
+        strokeWidth: widget.strokeWidth,
+      ),
+    );
+  }
+}
+
+class BorderPainter extends CustomPainter {
+  final Radius radius;
+  final double strokeWidth;
+  final Gradient gradient;
+  final _paint = Paint()..style = PaintingStyle.stroke;
+
+  BorderPainter(
+      {required this.gradient,
+      required this.radius,
+      required this.strokeWidth});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    final rrect = RRect.fromRectAndRadius(rect, radius);
+    _paint
+      ..strokeWidth = strokeWidth
+      ..shader = gradient.createShader(rect);
+    canvas.drawRRect(rrect, _paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
