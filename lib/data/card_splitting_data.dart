e4fe08f5fd4c479b8d7de76bd9b9542845d53fcb
Prosper AMEBE <prosperamebe14@gmail.com>
2024-11-27 11:52:30 -0500

    refactor: card splatting data sample created

---
+++ lib/data/card_splitting_data.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/splitting_Item_model.dart';
+
+final List<SplittingItemModel> items = [
+  SplittingItemModel(
+    title: "What is Interacting Design?",
+    detail: "Designing how users interact with digital interfaces with"
+        "intuitive experiences.",
+    icon: Icons.ads_click,
+  ),
+  SplittingItemModel(
+    title: "Principles & Patterns",
+    detail: "Fundamental guidelines and repeated solutions that ensure"
+        "consistency and usability  in design.",
+    icon: Icons.layers_outlined,
+  ),
+  SplittingItemModel(
+    title: "Usability & Accessibility",
+    detail: "Focusing on making digital designs easy to use and accessible"
+        "for everyone, including those with disabilities.",
+    icon: Icons.touch_app_outlined,
+  ),
+  SplittingItemModel(
+    title: "Prototyping & Testing",
+    detail: "Designing how users interact with digital interfaces with"
+        "intuitive experiences.",
+    icon: Icons.send_outlined,
+  ),
+  SplittingItemModel(
+    title: "UX Optimization",
+    detail: "Improving the overall user experience by enhancing usability"
+        "and satisfaction.",
+    icon: Icons.speed_outlined,
+  ),
+];
