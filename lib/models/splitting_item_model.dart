6ddfc6cdeda16f0c724e60fbe9ec2fde5c08b15d
Prosper AMEBE <prosperamebe14@gmail.com>
2024-11-27 11:51:19 -0500

    card splitting model

---
+++ lib/models/splitting_item_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplittingItemModel {
+  String title, detail;
+  IconData icon;
+
+  SplittingItemModel({
+    required this.title,
+    required this.detail,
+    required this.icon,
+  });
+}
